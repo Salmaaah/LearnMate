3165aabb95d17a5bb65a23d1c82cc256f13ed0ec
Salma Ahmid <salma.ahmid.el@gmail.com>
2023-12-23 18:06:34 -0500

    Improved the MenuItemcomponent
     Added support for submenus .Added tooltip on hover for iconOnly MenuItems and side display on hover for iconOnly MenuItems with submenus.

--- client/src/components/Shared/MenuItem/MenuItem.js
+++ client/src/components/Shared/MenuItem/MenuItem.js
@@ -1,52 +1,75 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { ReactComponent as MoreIcon } from '../../../assets/icons/more.svg';
 
-const MenuItem = ({
-  children,
-  to,
-  location,
-  icon,
-  label,
-  more,
-  expanded,
-  onClick,
-}) => {
-  const active = to === location;
-  // const [open, setOpen] = useState(false);
+const MenuItem = ({ children, to, icon, label, iconOnly, onClick }) => {
+  const location = useLocation();
+  const active = to === location.pathname;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    if (children) {
+      // in case the MenuItem has a submenu
+      setIsOpen(!isOpen);
+    }
+  };
+
+  useEffect(() => {
+    if (iconOnly) {
+      setIsOpen(false); // Close the submenu when the MenuItem is set to iconOnly
+    }
+  }, [iconOnly]);
 
   return (
-    <li>
+    <li
+      className={`menuItem ${
+        isOpen && !iconOnly // don't need !iconOnly because of the condition in useEffect
+          ? 'sub1'
+          : children && isHovered && iconOnly
+          ? 'sub2 tr-tl'
+          : ''
+      }`}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
       <Link
-        className={`menuItem ${active ? 'active' : ''}`}
+        className={`menuItem__title ${active ? 'active' : ''}`}
         to={to}
-        onClick={onClick}
-        // onClick={() => setOpen(!open)}
+        onClick={handleClick}
       >
         {icon}
-        <span className={`${expanded ? 'visible' : 'hidden'}`}>
+        <span className={`${iconOnly ? 'hidden' : 'visible'}`}>
           {label && <div>{label}</div>}
-          {more && <MoreIcon />}
-          {/* <ul className={`${open ? 'open' : 'closed'}`}>{children}</ul> */}
+          {children && <MoreIcon />}
         </span>
       </Link>
+      <ul className="menuItem__submenu">{children}</ul>
+      <div
+        className={`menuItem__tooltip ${
+          iconOnly && !children && isHovered ? 'visible' : 'hidden'
+        }`}
+      >
+        {label}
+      </div>
     </li>
   );
 };
 
 MenuItem.defaultProps = {
-  more: false,
-  expanded: true,
+  iconOnly: false,
 };
 
 MenuItem.propTypes = {
   to: PropTypes.string,
-  location: PropTypes.string,
-  // icon: PropTypes.string,
+  icon: PropTypes.string,
   label: PropTypes.string,
-  more: PropTypes.bool,
-  expanded: PropTypes.bool,
+  iconOnly: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default MenuItem;
