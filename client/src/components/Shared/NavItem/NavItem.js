58e13b0cc7030f30143a463cea8059afd63b89f6
Salma Ahmid <salma.ahmid.el@gmail.com>
2023-12-23 18:16:21 -0500

    Improved NavItem Component
    Added outside click support to close navitem submenu. Introduced position prop for submenu positioning via classes.

--- client/src/components/Shared/NavItem/NavItem.js
+++ client/src/components/Shared/NavItem/NavItem.js
@@ -1,16 +1,33 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useSidebar } from '../../../contexts/SidebarContext';
 
-const NavItem = ({ children, icon }) => {
+const NavItem = ({ children, icon, label, position }) => {
   const [open, setOpen] = useState(false);
+  const { isExpanded } = useSidebar(); // This will mess things up in cases where a navitem is not in a sidebar
+  const navItemRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (navItemRef.current && !navItemRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
 
   return (
-    <li>
-      <Link className="navItem" onClick={() => setOpen(!open)}>
-        {icon}
+    <li className="navItem" ref={navItemRef}>
+      <Link onClick={() => setOpen(!open)}>
+        <div className="navItem__button">{icon}</div>
+        <div className={`${isExpanded ? 'visible' : 'hidden'}`}>{label}</div>
       </Link>
-
-      {open && children}
+      {open && <ul className={`navItem__dropdown ${position}`}>{children}</ul>}
     </li>
   );
 };
